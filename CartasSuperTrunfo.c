d630a40befae5567b49fa870903f08ae1a1eee24
Eduardo_Dev <140466243+Eduardo-Dev-0@users.noreply.github.com>
2025-01-28 19:07:18 +0000

    Supertrunfo V1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,55 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    
+
+char Estado;
+
+char codigocarta[3];
+
+char nomecidade[12];
+
+float populacao;
+
+float areakm;
+
+float pib;
+
+int numerosturisticos;
+
+printf("Digite o Estado: \n", Estado);
+scanf("%c", &Estado);
+
+printf("Digite o codigo da carta: \n");
+scanf("%s", &codigocarta);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s", &nomecidade);
+
+printf("Digite sua população: \n");
+scanf("%f", &populacao);
+
+printf("Digite a área em km: \n");
+scanf("%f", &areakm);
+
+printf("Digite seu pib: \n");
+scanf("%f", &pib);
+
+printf("Digite as áreas turisticas: \n");
+scanf("%d", &numerosturisticos);
+
+printf("Nome do estado: %c \n", Estado);
+printf("Codigo da carta: %s \n", codigocarta);
+printf("Nome da cidade: %s \n", nomecidade);
+printf("População: %f \n", populacao);
+printf("area em km: %f \n", areakm);
+printf("pib: %f \n", pib);
+printf("numeros turisticos: %d \n", numerosturisticos);
+
+
+
+
+
+
     return 0;
 }
